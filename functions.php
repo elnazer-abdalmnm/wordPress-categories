60a20d3cfc64398f16acfac7e2bc95ef921f175d
Alnazeer Abdo <69108345+elnazer-abdalmnm@users.noreply.github.com>
2023-09-12 08:47:25 +0300

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+// In your theme's functions.php file or custom plugin file
+function show_categories_list() {
+    $categories = get_categories(); // Retrieve all categories
+
+    if ($categories) {
+        $output = '<div class="category-grid">';
+        foreach ($categories as $category) {
+            $category_image = z_taxonomy_image_url($category->term_id);
+
+            $output .= '<div class="category-wrapper">';
+            $output .= '<div class="category-image" style="background-image: url(' . esc_url($category_image) . ');"></div>';
+            $output .= '<div class="category-name"><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></div>';
+            $output .= '</div>';
+        }
+        $output .= '</div>';
+        return $output;
+    } else {
+        return 'No categories found.';
+    }
+}
+add_shortcode('categories', 'show_categories_list');
